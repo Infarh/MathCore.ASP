df9e6ece1774c0eba3feab1d5dad5598ebfc61aa
Infarh <shmachilin@yandex.ru>
2020-04-06 14:06:12 +0300

    Метод-расширение для контроллера, упрощающий выполнение действий с ним

---
+++ MathCore.ASP/Extensions/ControllerBaseExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using MathCore.ASP.Annotations;
+// ReSharper disable UnusedType.Global
+
+// ReSharper disable once CheckNamespace
+namespace Microsoft.AspNetCore.Mvc
+{
+    public static class ControllerBaseExtensions
+    {
+        public static T With<T>([NotNull] this T controller, [NotNull] Action<T> action)
+            where T : ControllerBase
+        {
+            if (action is null) throw new ArgumentNullException(nameof(action));
+            action(controller ?? throw new ArgumentNullException(nameof(controller)));
+            return controller;
+        }
+    }
+}
